90d705aa5bc3c1e2e2c6ccd02194a9f9a9c96e72
nehaumbre <nehaumbre28@gmail.com>
2023-09-09 16:16:50 +0530

    styling icons

---
+++ Foods/script.js
@@ -0,0 +1,7 @@
+const icons = document.querySelectorAll('.section-1-icons i')
+
+setTimeout(()=>{
+    const icon = document.querySelector('.section-1-icons.change')
+
+    icon.classList.remove('change')
+}, 4000)
